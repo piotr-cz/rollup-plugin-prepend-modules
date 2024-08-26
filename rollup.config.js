21ac1fa48a537c69e899388e63ad7841a1be2838
Piotr Konieczny <hello@piotr.cz>
2024-08-26 09:29:25 +0200

    Add dev types

--- rollup.config.js
+++ rollup.config.js
@@ -5,11 +5,12 @@ const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 const externalDeps = Object.keys(pkg.dependencies);
 const nodeDeps = ['path'];
 
+/** @type {import('rollup').RollupOptions} */
 export default {
   input: 'src/index.js',
   external: [...externalDeps, ...nodeDeps],
   output: [
+    { file: pkg.module, format: 'es' },
     { file: pkg.main, format: 'cjs', exports: 'auto' },
-    { file: pkg.module, format: 'es' }
-  ]
+  ],
 };
