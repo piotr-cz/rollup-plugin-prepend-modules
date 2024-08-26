a18f493eb4a76236a9274c84e057c9624dafa77b
Piotr Konieczny <hello@piotr.cz>
2024-08-26 09:55:52 +0200

    Use rollup convention

---
+++ rollup.config.mjs
@@ -0,0 +1,17 @@
+import { readFileSync } from 'node:fs';
+import { builtinModules } from 'module';
+
+const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf8'));
+
+/** @type {import('rollup').RollupOptions} */
+export default {
+  input: 'src/index.js',
+  // external: [...externalDeps, ...nodeDeps],
+  external: Object.keys(pkg.dependencies)
+    .concat(Object.keys(pkg.peerDependencies))
+    .concat(builtinModules),
+  output: [
+    { format: 'esm', file: pkg.module },
+    { format: 'cjs', file: pkg.main },
+  ],
+};
